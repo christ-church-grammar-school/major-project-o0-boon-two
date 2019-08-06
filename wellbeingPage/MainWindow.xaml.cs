637c8538921f48b6f150452d8b9b863a27cbb02a
Isaac Ching (Y10) <1012543@student.ccgs.wa.edu.au>
2019-08-06 14:49:19 +0800

    Add project files.

---
+++ wellbeingPage/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wellbeingPage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Clicked(object sender, RoutedEventArgs e)
+        {
+            ShowWellbeingPage();
+        }
+
+        private void ShowWellbeingPage()
+        {
+            wellbeing page = new wellbeing();
+            var contentCopy = Content;
+            Content = page;
+            page.ladder += (object sender, EventArgs e) =>
+            {
+                Content = contentCopy;
+            };
+        }
+    }
+}
---
+++ wellbeingPage/wellbeing.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wellbeingPage
+{
+    /// <summary>
+    /// Interaction logic for wellbeing.xaml
+    /// </summary>
+    public partial class wellbeing : Page
+    {
+        public EventHandler ladder;
+
+        public wellbeing()
+        {
+            InitializeComponent();
+        }
+
+        public void Climbladder()
+        {
+            ladder(this, EventArgs.Empty);
+        }
+
+        private void addClicked(object sender, RoutedEventArgs e)
+        {
+            opacityRectangle.Width = 1200;
+            addPopupBottom.Width = 1000;
+            addPopupTop.Width = 1000;
+            addCloseButton.Width = 80;
+        }
+
+        private void addCloseClicked(object sender, RoutedEventArgs e)
+        {
+            opacityRectangle.Width = 0;
+            addPopupBottom.Width = 0;
+            addPopupTop.Width = 0;
+            addCloseButton.Width = 0;
+        }
+
+        private void infoClicked(object sender, RoutedEventArgs e)
+        {
+            opacityRectangle.Width = 1200;
+            infoPopupBottom.Width = 1000;
+            infoPopupTop.Width = 1000;
+            infoCloseButton.Width = 80;
+        }
+
+        private void infoCloseClicked(object sender, RoutedEventArgs e)
+        {
+            opacityRectangle.Width = 0;
+            infoPopupBottom.Width = 0;
+            infoPopupTop.Width = 0;
+            infoCloseButton.Width = 0;
+        }
+    }
+}
