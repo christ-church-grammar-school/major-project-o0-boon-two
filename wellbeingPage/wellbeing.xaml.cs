58417f4eb609e7569ec6c6baeacb42e8667c69ba
Isaac Ching <1012543@student.ccgs.wa.edu.au>
2019-08-13 09:04:50 +0800

    Added useless comment

--- wellbeingPage/wellbeing.xaml.cs
+++ wellbeingPage/wellbeing.xaml.cs
@@ -18,6 +18,8 @@ namespace wellbeingPage
     /// <summary>
     /// Interaction logic for wellbeing.xaml
     /// </summary>
+    /// 
+    /// I hope everyone can see this?
     public partial class wellbeing : Page
     {
         public EventHandler ladder;
